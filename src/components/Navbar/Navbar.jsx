a148aff83a7bd9458d434fdcddafb8862bf0b88e
usmansajid00 <usmansajid2517@gmail.com>
2024-06-07 09:54:37 +0500

    add navbar functionality

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,46 @@
 import { useState } from "react";
 import { assets } from "../../assets/assets";
 import "./navbar.scss";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  const [activeMenu, setActiveMenu] = useState("Home");
-
-  const menuItems = ["Home", "Menu", "Mobile App", "Contact Us"];
+  const [menu, setMenu] = useState("Home");
 
   return (
     <div className="navbar">
-      {/* <img src={assets.logo} className="logo" alt="logo" /> */}
-      <h1 className="logo">Foodie baba</h1>
+      <img src={assets.logo} className="logo" />
       <ul className="navbar-menu">
-        {menuItems.map((item) => (
-          <li
-            key={item}
-            onClick={() => setActiveMenu(item)}
-            className={activeMenu === item ? "active" : ""}
-          >
-            {item}
-          </li>
-        ))}
+        <Link
+          to="/"
+          onClick={() => setMenu("Home")}
+          className={menu === "Home" ? "active" : ""}
+        >
+          Home
+        </Link>
+        <a
+          href="#explore-menu"
+          onClick={() => setMenu("Menu")}
+          className={menu === "Menu" ? "active" : ""}
+        >
+          Menu
+        </a>
+        <a
+          href="#app-download"
+          onClick={() => setMenu("Mobile App")}
+          className={menu === "Mobile App" ? "active" : ""}
+        >
+          Mobile App
+        </a>
+        <a
+          href="#footer"
+          onClick={() => setMenu("Contact Us")}
+          className={menu === "Contact Us" ? "active" : ""}
+        >
+          Contact Us
+        </a>
       </ul>
       <div className="navbar-right">
-        <img src={assets.search_icon} alt="search" />
+        <img src={assets.search_icon} />
         <div className="navbar-search-icon">
           <img src={assets.basket_icon} alt="basket" />
           <div className="dot"></div>
